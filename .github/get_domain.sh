847e8555ee164bd2fc1dcaf9dc864d44197a793a
hamasakiharuya <hamasakiharuya@MacBook-Pro.local>
2023-01-04 20:37:38 +0900

    add get domain

--- .github/get_domain.sh
+++ .github/get_domain.sh
@@ -8,7 +8,7 @@ USER_POOL_CHRCK=$?
 
 if [ ${USER_POOL_CHRCK} -eq 0 ]; then
   CLOUDFRONT_CNAME=`aws cloudfront list-distributions --max-items 1 --output table | grep "CNAME" | cut -d "|" -f6 | tr -d ' '`
-  echo ${CLOUDFRONT_CNAME}
+  echo "CNAME: ${CLOUDFRONT_CNAME}"
 else
   echo "ERROR: CloudFront does not exist"
   exit 1
@@ -18,7 +18,7 @@ fi
 if [[ "${CLOUDFRONT_CNAME}" == *mymovie.jp ]]; then
   for i in ${TARGET_FILE_LIST[@]}
   do
-    sed -i 's/'¥{DOMAIN¥}'/'${CLOUDFRONT_CNAME}'/' $i
+    sed -i 's/'{DOMAIN}'/'${CLOUDFRONT_CNAME}'/' $i
     grep -rl '${CLOUDFRONT_CNAME}' $i
   done
 fi
